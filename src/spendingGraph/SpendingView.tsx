7e4a0a8279e55451e933b3aaaaab2e16e8c5bb46
Tony Martinez <jm3287185@gmail.com>
2024-09-21 12:27:52 -0500

    Moved Spending data

--- src/spendingGraph/SpendingView.tsx
+++ src/spendingGraph/SpendingView.tsx
@@ -1,43 +1,35 @@
 import * as M from "@mui/material";
+import { yellow } from "@mui/material/colors";
 import * as React from "react";
 import { useState } from "react";
 
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ReferenceLine } from 'recharts';
 
-const data = [
-    { name: 'Jan', spending: 65 },
-    { name: 'Feb', spending: 59 },
-    { name: 'Mar', spending: 80 },
-    { name: 'Apr', spending: 81 },
-    { name: 'May', spending: 56 },
-    { name: 'Jun', spending: 55 },
-    { name: 'Jul', spending: 40 },
-    { name: 'Aug', spending: 48 },
-    { name: 'Sep', spending: 38 },
-    { name: 'Oct', spending: 39 },
-    { name: 'Nov', spending: 48 },
-    { name: 'Dec', spending: 38 },
-];
+interface Props {
+    spending: Array<any>;
+}
 
-
-
-
-const SpendingView: React.FC = () => {
+const SpendingView: React.FC<Props> = ( { spending } ) => {
     return (
         <M.Container>
             <M.Typography variant="h4" gutterBottom>
                 Spending
             </M.Typography>
 
-
             <ResponsiveContainer width="100%" height={400}>
-                <BarChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                <BarChart data={spending} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="name" />
                     <YAxis />
                     <Tooltip />
-                    
                     <Bar dataKey="spending" fill="#8884d8" />
+                    
+
+                    // Bar with multiple colors
+
+                    <ReferenceLine y={30} label="Goal" stroke="yellow" strokeDasharray="3 3" />
+                    <ReferenceLine y={90} label="Income" stroke="red" strokeDasharray="3 3" />
+
                 </BarChart>
             </ResponsiveContainer>
         </M.Container>
